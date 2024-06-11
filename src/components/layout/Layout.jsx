1e194035fa0b2cc708da6d2050150819362dc1ae
Eric Pietrowicz <pietrowicz.eric@gmail.com>
2024-06-11 08:21:36 -0500

    cta

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import SolidButton from '../ui/SolidButton'
-import { BluetoothIcon, BluetoothOff } from 'lucide-react'
+import { BluetoothIcon, BluetoothOff, XIcon } from 'lucide-react'
 import { useBluetooth } from '../../hooks/useBluetooth'
 import TextInput from '../ui/TextInput'
 
 const Layout = ({ children }) => {
+  const [ctaOpen, setCtaOpen] = useState(true)
+
   return (
-    <div className='flex items-start'>
+    <div className='flex items-start relative'>
       <Sidebar />
       <div className='flex-1 min-h-screen flex flex-col justify-start'>
         <Header />
         {children}
       </div>
+
+      {ctaOpen && (
+        <div className='absolute bottom-0 right-0 p-4 m-4 border max-w-72'>
+          <h2 className='font-semibold mb-2'>Thank you 🤝</h2>
+          <button onClick={() => setCtaOpen(false)} className='absolute top-4 right-4 '>
+            <XIcon className='w-4 h-4 text-gray-500' />
+          </button>
+
+          <p className='text-sm text-gray-600 mb-1'>I appreciate you giving Blueprint a try!</p>
+          <p className='text-sm text-gray-600'>
+            It's a work in progress. If you have any feedback please send it my way at{' '}
+            <a className='text-blue-500 underline' href='mailto:ubiqueiot@gmail.com'>ubiqueiot@gmail.com</a>
+          </p>
+        </div>
+      )}
     </div>
   )
 }
